013a9b2361fd0c979f01866261def63c9bfc7e96
Nikita Buinovskyi <nikita.buinovskyi@student.tuke.sk>
2025-08-07 22:18:43 +0200

    cors error handled

--- OrgStructureApi/Program.cs
+++ OrgStructureApi/Program.cs
@@ -9,10 +9,24 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.MapControllers();
 
